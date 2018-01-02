3470b5b99348c9912072745b1a3e925af47af874
wshaol <1129421353@qq.com>
2018-01-02 17:08:48 +0800

    https://github.com/wshaol/hello175.git

---
+++ src/Demo/java/one.java
@@ -0,0 +1,22 @@
+package Demo.java;
+
+import java.util.Scanner;
+
+public class one {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	     Scanner sc=new Scanner(System.in);
+	     System.out.println("请输入一个数");
+	     System.out.println("请输入一个数");
+	     System.out.println("请输入一个数");
+	     System.out.println("请输入一个数");
+	     System.out.println("请输入一个数");
+	     int a=sc.nextInt();
+
+	     
+	     
+		}
+	}
+
+
